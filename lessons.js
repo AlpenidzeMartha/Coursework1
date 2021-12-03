c9a681ccd4d98d65da34397f962784f4ad4cb3fb
Martha Alpenidze <alpenidzemartha1@gmail.com>
2021-12-03 12:12:15 +0400

    Lessons data

---
+++ lessons.js
@@ -0,0 +1,84 @@
+let lessons = 
+[    
+    {
+        "id": 1001,
+        "subject": "Math",
+        "location": "London",
+        "price": 200,
+        "image": "math.png",
+        "availablePlaces": 5  
+    },
+    {
+        "id": 1002,
+        "subject": "English",
+        "location": "Bristol",
+        "price": 100,
+        "image": "eng.jpg",
+        "availablePlaces": 5
+        },
+    {
+        "id": 1003,
+        "subject": "Music",
+        "location": "Liverpool",
+        "price": 150,
+        "image": "music.jpg",
+        "availablePlaces": 5
+    },
+    {
+        "id": 1004,
+        "subject": "Geography",
+        "location": "Birmingham",
+        "price": 100,
+        "image": "geo.jpg",
+        "availablePlaces": 5
+        },
+    {
+        "id": 1005,
+        "subject": "Chemistry",
+        "location": "Cambridge",
+        "price": 200,
+        "image": "chem.png",
+        "availablePlaces": 5
+    },
+    {
+        "id": 1006,
+        "subject": "Physics",
+        "location": "Oxford",
+        "price": 200,
+        "image": "phy.png",
+        "availablePlaces": 5
+    },
+    {
+        "id": 1007,
+        "subject": "Biology",
+        "location": "Leeds",
+        "price": 200,
+        "image": "bio.png",
+        "availablePlaces": 5
+        },
+    {
+        "id": 1008,
+        "subject": "Literature",
+        "location": "London",
+        "price": 100,
+        "image": "lit.png",
+        "availablePlaces": 5
+    },
+    {
+        "id": 1009,
+        "subject": "History",
+        "location": "Sheffield",
+        "price": 150,
+        "image": "history.jpg",
+        "availablePlaces": 5
+        },
+    {
+        "id": 1010,
+        "subject": "Physical Education",
+        "location": "Liverpool",
+        "price": 100,
+        "image": "PE.png",
+        "availablePlaces": 5
+    }
+    
+    ]
